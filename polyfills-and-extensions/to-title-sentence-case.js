25036689cf0c8cf43998698986243f371f8a3152
ryanbmarx <ryanbmarx@gmail.com>
2018-01-11 11:42:09 -0600

    do not cap aka

--- polyfills-and-extensions/to-title-sentence-case.js
+++ polyfills-and-extensions/to-title-sentence-case.js
@@ -8,7 +8,7 @@ String.prototype.toSentenceCase = function(){
 String.prototype.toTitleCase = function(){
 	// returns string in title case (Every word capitalized)
 	const words_arr = this.split(' '),
-		doNotCap = ["a", "an", "the", "and", "but", "or", "for", "nor", "etc.", "on", "at", "to", "from", "by", "with"];
+		doNotCap = ["a", "an", "aka", "a.k.a.", "the", "and", "but", "or", "for", "nor", "etc.", "on", "at", "to", "from", "by", "with"];
 
 	let retval="";
 
